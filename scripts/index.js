a300ea9bdc8c9bc35bda082860a253d5e17887e9
Denis Pikiner <Toribashzmdog@mail.ru>
2023-06-07 09:25:59 +0300

    Поправил target

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 let popUpModal = document.querySelector(".popup"),
     buttons = [
         {element: document.querySelectorAll('.elements__elements-grid'), event: 'click', func: likeStatus},
-        {element: document.querySelectorAll('.popup__save-button'), event: 'submit', func: popUpSaveChanges},
+        {element: document.querySelectorAll('.popup__container'), event: 'submit', func: popUpSaveChanges},
         {element: document.querySelectorAll('.profile__edit-button'), event: 'click', func: popUpOpen},
         {element: document.querySelectorAll('.popup__close-button'), event: 'click', func: popUpState},
         {
@@ -9,7 +9,12 @@ let popUpModal = document.querySelector(".popup"),
             event: 'click',
             func: () => console.log("В разработке")
         },
-    ]
+    ];
+const
+    PROFILE_NAME = document.querySelector(".profile__name"),
+    PROFILE_STATUS = document.querySelector(".profile__status"),
+    PROFILE_NAME_POPUP = document.querySelector(".popup__edit[name='name']"),
+    PROFILE_STATUS_POPUP = document.querySelector(".popup__edit[name='status']");
 
 /* Перебор массива с параметрами(elem: массив кнопок с классом и func: используемая ими функция) */
 buttons.map((elemArr) => {
@@ -17,48 +22,42 @@ buttons.map((elemArr) => {
     /*далее перебираю массив кнопок с классами и присваиваю им функцию из массива с параметрами*/
     elemArr.element.forEach((elemClass) => {
 
-        elemClass.addEventListener(elemArr.event, () => elemArr.func())
+        elemClass.addEventListener(elemArr.event, (event) => elemArr.func(event));
     })
 })
-
-/* Подвязка слушателя к таблице */
-document.querySelector('.elements__elements-grid').addEventListener('click', likeStatus)
 
 /* Переключатель для лайков. Выводит ошибку*/
 function likeStatus (event) {
 
-    if (event.target.classList.contains('element__like')) event.target.classList.toggle('element__like_active')
+    if (event.target.classList.contains('element__like')) event.target.classList.toggle('element__like_active');
 }
 
 /*Функция скрывает или показывает попАп */
 function popUpState() {
-    document.querySelector('.popup_opened')?
-        popUpModal.classList.remove('popup_opened') :
-        popUpModal.classList.add('popup_opened');
+
+    popUpModal.classList.contains('popup_opened')?
+        popUpModal.classList.toggle('popup_opened') :
+        popUpModal.classList.toggle('popup_opened');
 }
 
 /*Функция Меняет содержимое инпутов и отображает попап */
 function popUpOpen() {
-    let profileName = document.querySelector(".profile__name"),
-        profileStatus = document.querySelector(".profile__status");
 
     /* Меняю содержимое инпутов */
-    document.querySelector(".popup__edit[name='name']").value = profileName.textContent;
-    document.querySelector(".popup__edit[name='status']").value = profileStatus.textContent;
+    document.querySelector(".popup__edit[name='name']").value = PROFILE_NAME.textContent;
+    document.querySelector(".popup__edit[name='status']").value = PROFILE_STATUS.textContent;
 
     /* Отображение попАпа*/
     popUpState();
 }
 
 /* Функция меняет имя и статус, а также скрывает попАп */
-function popUpSaveChanges() {
-
-    let profileName = document.querySelector(".popup__edit[name='name']"),
-        profileStatus = document.querySelector(".popup__edit[name='status']");
+function popUpSaveChanges(event) {
+    event.preventDefault()
 
     /* Меняю данные профиля */
-    document.querySelector(".profile__name").innerHTML = profileName.value;
-    document.querySelector(".profile__status").innerHTML = profileStatus.value;
+    document.querySelector(".profile__name").textContent = PROFILE_NAME_POPUP.value;
+    document.querySelector(".profile__status").textContent = PROFILE_STATUS_POPUP.value;
 
     /* Скрываю попап */
     popUpState();
