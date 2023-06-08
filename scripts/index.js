015924957bd28e0a71acbb5db62312af839fc7e5
Denis Pikiner <Toribashzmdog@mail.ru>
2023-06-08 08:13:33 +0300

    Поправил target

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 let popUpModal = document.querySelector(".popup"),
     buttons = [
-        {element: document.querySelectorAll('.elements__elements-grid'), event: 'click', func: likeStatus},
+        {element: document.querySelectorAll('.elements__elements-grid'), event: 'click', func: toggleLikeStatus},
         {element: document.querySelectorAll('.popup__container'), event: 'submit', func: popUpSaveChanges},
-        {element: document.querySelectorAll('.profile__edit-button'), event: 'click', func: popUpOpen},
-        {element: document.querySelectorAll('.popup__close-button'), event: 'click', func: popUpState},
+        {element: document.querySelectorAll('.profile__edit-button'), event: 'click', func: openPopUp},
+        {element: document.querySelectorAll('.popup__close-button'), event: 'click', func: checkPopUpState},
         {
             element: document.querySelectorAll('.profile__add-button'),
             event: 'click',
@@ -11,10 +11,10 @@ let popUpModal = document.querySelector(".popup"),
         },
     ];
 const
-    PROFILE_NAME = document.querySelector(".profile__name"),
-    PROFILE_STATUS = document.querySelector(".profile__status"),
-    PROFILE_NAME_POPUP = document.querySelector(".popup__edit[name='name']"),
-    PROFILE_STATUS_POPUP = document.querySelector(".popup__edit[name='status']");
+    profileName = document.querySelector(".profile__name"),
+    profileStatus = document.querySelector(".profile__status"),
+    profileNamePopUp = document.querySelector(".popup__edit[name='name']"),
+    profileStatusPopUp = document.querySelector(".popup__edit[name='status']");
 
 /* Перебор массива с параметрами(elem: массив кнопок с классом и func: используемая ими функция) */
 buttons.map((elemArr) => {
@@ -26,39 +26,37 @@ buttons.map((elemArr) => {
     })
 })
 
-/* Переключатель для лайков. Выводит ошибку*/
-function likeStatus (event) {
+/* Переключатель для лайков */
+function toggleLikeStatus (event) {
 
     if (event.target.classList.contains('element__like')) event.target.classList.toggle('element__like_active');
 }
 
 /*Функция скрывает или показывает попАп */
-function popUpState() {
+function checkPopUpState() {
 
-    popUpModal.classList.contains('popup_opened')?
-        popUpModal.classList.toggle('popup_opened') :
-        popUpModal.classList.toggle('popup_opened');
+    popUpModal.classList.toggle('popup_opened');
 }
 
 /*Функция Меняет содержимое инпутов и отображает попап */
-function popUpOpen() {
+function openPopUp() {
 
     /* Меняю содержимое инпутов */
-    document.querySelector(".popup__edit[name='name']").value = PROFILE_NAME.textContent;
-    document.querySelector(".popup__edit[name='status']").value = PROFILE_STATUS.textContent;
+    profileNamePopUp.value = profileName.textContent;
+    profileStatusPopUp.value = profileStatus.textContent;
 
     /* Отображение попАпа*/
-    popUpState();
+    checkPopUpState();
 }
 
 /* Функция меняет имя и статус, а также скрывает попАп */
 function popUpSaveChanges(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     /* Меняю данные профиля */
-    document.querySelector(".profile__name").textContent = PROFILE_NAME_POPUP.value;
-    document.querySelector(".profile__status").textContent = PROFILE_STATUS_POPUP.value;
+    profileName.textContent = profileNamePopUp.value;
+    profileStatus.textContent = profileStatusPopUp.value;
 
     /* Скрываю попап */
-    popUpState();
+    checkPopUpState();
 }
