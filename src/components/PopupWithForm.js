4c8335e45aa19e4f67f0be77bee8adedce4427fe
Denis Pikiner <Toribashzmdog@mail.ru>
2023-08-14 13:02:29 +0300

    Завершение

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -3,20 +3,22 @@ import Popup from "./Popup";
 export default class PopupWithForm extends Popup {
     constructor({selector, handlerSubmit}) {
         super(selector);
-        /* Для передачи параметра(функции) делаю привязку с параметром к пустому контексту, далее в index.js вызываю функцию(параметр) в контексте необходимого объекта */
-        this._handlerSubmit = handlerSubmit.bind("", () => this._getInputValues)
+
+        this._handlerSubmit = () => {
+            handlerSubmit(this._getInputValues)
+        }
 
         this._form = this.popup.querySelector('.popup__container')
         this._inputList = this._form.querySelectorAll('.popup__edit')
-        this._formValues = {}
+        this.formValues = {}
     }
 
     _getInputValues() {
 
         this._inputList.forEach(input => {
-            this._formValues[input.name] = input.value;
+            this.formValues[input.name] = input.value;
         });
-        return this._formValues;
+        return this.formValues;
     }
 
     _setInputValues(data) {
@@ -38,7 +40,7 @@ export default class PopupWithForm extends Popup {
     }
 
     open(preOpenPopup) {
-        if (preOpenPopup()) this._setInputValues(preOpenPopup())
+        if (preOpenPopup) this._setInputValues(preOpenPopup())
         super.open()
     }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import UserInfo from "../components/UserInfo";
 /* объекты*/
 const formValidators = {}
 
-const enableValidation = (config) => {
+function enableValidation(config) {
     const formList = Array.from(document.querySelectorAll(config.formSelector))
     formList.forEach((formElement) => {
         const validator = new FormValidator(config, formElement)
@@ -20,44 +20,50 @@ const enableValidation = (config) => {
         formValidators[formName] = validator;
         validator.enableValidation();
     });
-};
+}
+
+function createCard(item) {
+    const cardElement = new Card(item, ".element-template", () => popUpModalImage.open(item.name, item.link))
+
+    return cardElement.renderCard()
+}
 
 const userInformation = new UserInfo({selectorName: ".profile__name", selectorStatus: ".profile__status"})
 
 const popUpModalProfile = new PopupWithForm({
     selector: ".popup_profile",
     handlerSubmit: (callBack) => {
-        formValidators.changeProfile.checkValidation()
-        callBack().call(popUpModalProfile)
-        userInformation.setUserInfo(popUpModalProfile._formValues)
+        callBack.call(popUpModalProfile)
+        userInformation.setUserInfo(popUpModalProfile.formValues)
         popUpModalProfile.close()
     }
 })
+
 const popUpModalImage = new PopupWithImage(".popup_image")
 /* Отрисовка всех имеющихся карточек из массива initialCards */
 const renderCards = new Section({
     items: constants.cards,
     render: (data) => {
-        const card = new Card(data, ".element-template", event => popUpModalImage.open(
-            event.target.closest('.element').querySelector('.element__title').textContent,
-            event.target.src))
-        renderCards.addItem(card.renderCard())
+        const card = createCard(data)
+
+        renderCards.addItem(card)
     }
 }, ".elements__elements-grid")
 
 const popUpModalPlace = new PopupWithForm({
-    selector: ".popup_place",
-    handlerSubmit:(callBack) => {
-        const card = new Card(callBack().call(popUpModalPlace), ".element-template", event => popUpModalImage.open(
-            event.target.closest('.element').querySelector('.element__title').textContent,
-            event.target.src))
-        renderCards.addItem(card.renderCard())
-        formValidators.addPlace.toggleButtonState()
-        popUpModalPlace.close()
-    }}
+        selector: ".popup_place",
+        handlerSubmit: (callBack) => {
+            const card = createCard(callBack.call(popUpModalPlace))
 
+            renderCards.addItem(card)
+            popUpModalPlace.close()
+        }
+    }
 )
-constants.profileAddButton.addEventListener('click', () => popUpModalPlace.open(() => formValidators.addPlace.toggleButtonState()));
+constants.profileAddButton.addEventListener('click', () => {
+    popUpModalPlace.open()
+    formValidators.addPlace.toggleButtonState()
+})
 constants.profileEditButton.addEventListener('click', () => popUpModalProfile.open(() => userInformation.getUserInfo()));
 
 
