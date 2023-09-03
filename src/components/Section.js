64a95dccdc9d5d76c8a3d8314b3251228fb304d5
Denis Pikiner <Toribashzmdog@mail.ru>
2023-09-03 23:28:29 +0300

    Завершение

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,6 @@
 export default class Section {
-    constructor(selector) {
-
+    constructor(selector, render) {
+        this._render = render
         this._container = document.querySelector(selector);
     }
 
@@ -8,13 +8,17 @@ export default class Section {
         this._container.append(card)
     }
 
+    render(card, info) {
+        return this._render(card, info)
+    }
     addItemPrepend(card) {
         this._container.prepend(card)
     }
 
-    renderItems(cards) {
+    renderItems(cards, info) {
+
         cards.forEach((card) => {
-            this.addItemAppend(card)
+            this.addItemAppend(this.render(card, info))
         })
     }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import {api} from "../components/Api"
 
 
 const formValidators = {}
-const renderCards = new Section(".elements__elements-grid")
+let fetchedInfo
 
 function enableValidation(config) {
     const formList = Array.from(document.querySelectorAll(config.formSelector))
@@ -24,136 +24,133 @@ function enableValidation(config) {
         validator.enableValidation();
     });
 }
+function initialisationPopups() {
+    popUpModalPlace.initialisationPopup()
+    popUpModalProfile.initialisationPopup()
+    popUpModalAvatar.initialisationPopup()
+    popUpModalDeleteCard.initialisationPopup()
+    popUpModalImage.initialisationPopup()
+}
+const userInformation = new UserInfo({
+    selectorName: ".profile__name",
+    selectorStatus: ".profile__status",
+    selectorAvatar: '.profile__avatar'
+})
+const renderCards = new Section(".elements__elements-grid", (item, info) => {
+    const cardElement = new Card({item, info}, ".element-template",
+        () => popUpModalImage.open(item.name, item.link),
+        () => popUpModalDeleteCard.open(cardElement),
+        () => {
+            if (cardElement.getLikeStatus()) {
+                api.removeLike(cardElement.getId())
+                    .then((card) => {
+                        cardElement.removeLike(card)
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    })
+
+
+            } else {
+                api.makeLike(cardElement.getId())
+                    .then((card) => {
+                        cardElement.makeLike(card)
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    })
+            }
+
+        }
+    )
+
+    return cardElement.renderCard(item)
+})
+const popUpModalPlace = new PopupWithForm({
+    selector: ".popup_place",
+    handlerSubmit: infoProfile => {
+        formValidators.addPlace.fetchButton()
+        api.postCard(infoProfile)
+            .then(result => {
+                renderCards.addItemPrepend(renderCards.render(result, fetchedInfo))
+                popUpModalPlace.close()
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            .finally(() => formValidators.addPlace.fetchedButton('Создать'))
+    }
+})
+const popUpModalProfile = new PopupWithForm({
+    selector: ".popup_profile",
+    handlerSubmit: info => {
+        formValidators.changeProfile.fetchButton()
+        api.setInfoProfile(info)
+            .then((res) => {
+                userInformation.renderingInfoProfile(res)
+                popUpModalProfile.close()
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            .finally(() => formValidators.changeProfile.fetchedButton('Сохранить'))
+    }
+})
+const popUpModalImage = new PopupWithImage(".popup_image")
+const popUpModalAvatar = new PopupWithForm({
+    selector: ".popup_avatar",
+    handlerSubmit: info => {
+        formValidators.changeAvatar.fetchButton()
+        api.setInfoAvatar(info)
+            .then((res) => {
+                userInformation.renderingInfoProfile(res)
+                popUpModalAvatar.close()
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            .finally(() => formValidators.changeAvatar.fetchedButton('Сохранить'))
+    }
+})
+const popUpModalDeleteCard = new PopupDeleteCard({
+    selector: ".popup_delete",
+    handlerSubmit: () => {
+        api.deleteCard(popUpModalDeleteCard.card.getId())
+            .then(() => {
+                popUpModalDeleteCard.card.deleteCard()
+                popUpModalDeleteCard.close()
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+})
+constants.profileAddButton.addEventListener('click', () => {
+    popUpModalPlace.open()
+    formValidators.addPlace.toggleButtonState()
+})
+constants.profileEditButton.addEventListener('click', () => {
+    popUpModalProfile.setInputValues(userInformation.getUserInfo())
+    popUpModalProfile.open()
+});
+constants.profileChangeButton.addEventListener('click', () => {
+    popUpModalAvatar.open()
+    formValidators.changeAvatar.toggleButtonState()
+})
 
 Promise.all([
     api.getInfoProfile(),
     api.getInitialCards()
 ])
     .then(([info, cards]) => {
-        const userInformation = new UserInfo({
-            selectorName: ".profile__name",
-            selectorStatus: ".profile__status",
-            selectorAvatar: '.profile__avatar'
-        })
+        fetchedInfo = info
 
-        const popUpModalPlace = new PopupWithForm({
-            selector: ".popup_place",
-            handlerSubmit: infoProfile => {
-                formValidators.addPlace.fetchButton()
-                api.postCard(infoProfile)
-                    .then(result => {
-                        const card = createCard(result, info)
-                        renderCards.addItemPrepend(card)
-                        popUpModalPlace.close()
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    })
-                    .finally(() => formValidators.addPlace.fetchedButton('Создать'))
-            }
-        })
-        const popUpModalProfile = new PopupWithForm({
-            selector: ".popup_profile",
-            handlerSubmit: info => {
-                formValidators.changeProfile.fetchButton()
-                api.setInfoProfile(info)
-                    .then((res) => {
-                        userInformation.renderingInfoProfile(res)
-                        popUpModalProfile.close()
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    })
-                    .finally(() => formValidators.changeProfile.fetchedButton('Сохранить'))
-            }
-        })
-        const popUpModalImage = new PopupWithImage(".popup_image")
-        const popUpModalAvatar = new PopupWithForm({
-            selector: ".popup_avatar",
-            handlerSubmit: info => {
-                formValidators.changeAvatar.fetchButton()
-                api.setInfoAvatar(info)
-                    .then((res) => {
-                        userInformation.renderingInfoProfile(res)
-                        popUpModalAvatar.close()
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    })
-                    .finally(() => formValidators.changeAvatar.fetchedButton('Сохранить'))
-            }
-        })
-        const popUpModalDeleteCard = new PopupDeleteCard({
-            selector: ".popup_delete",
-            handlerSubmit: () => {
-                api.deleteCard(popUpModalDeleteCard.card.getId())
-                    .then(() => {
-                        popUpModalDeleteCard.card.deleteCard()
-                        popUpModalDeleteCard.close()
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    })
-            }
-        })
-        constants.profileAddButton.addEventListener('click', () => {
-            popUpModalPlace.open()
-            formValidators.addPlace.toggleButtonState()
-        })
-        constants.profileEditButton.addEventListener('click', () => {
-            popUpModalProfile.setInputValues(userInformation.getUserInfo())
-            popUpModalProfile.open()
-        });
-        constants.profileChangeButton.addEventListener('click', () => {
-            popUpModalAvatar.open()
-            formValidators.changeAvatar.toggleButtonState()
-        })
-
-        function createCard(item, info) {
-
-            const cardElement = new Card({item, info}, ".element-template",
-                () => popUpModalImage.open(item.name, item.link),
-                () => popUpModalDeleteCard.open(cardElement),
-                () => {
-                    if (cardElement.getLikeStatus()) {
-                        api.removeLike(cardElement.getId())
-                            .then((card) => {
-                                cardElement.removeLike(card)
-                            })
-                            .catch((err) => {
-                                console.log(err);
-                            })
-
-
-                    } else {
-                        api.makeLike(cardElement.getId())
-                            .then((card) => {
-                                cardElement.makeLike(card)
-                            })
-                            .catch((err) => {
-                                console.log(err);
-                            })
-                    }
-
-                }
-            )
-
-            return cardElement.renderCard(item)
-        }
-
-        popUpModalPlace.initialisationPopup()
-        popUpModalProfile.initialisationPopup()
-        popUpModalAvatar.initialisationPopup()
-        popUpModalDeleteCard.initialisationPopup()
-        popUpModalImage.initialisationPopup()
-        userInformation.renderingInfoProfile(info)
-        return cards.map(elem => {
-            return createCard(elem, info)
-        })
+        userInformation.renderingInfoProfile(fetchedInfo)
+        renderCards.renderItems(cards, fetchedInfo)
     })
-    .then(cards => renderCards.renderItems(cards))
     .catch((err) => {
         console.log(err);
     })
 
 enableValidation(constants.validatyParams);
+initialisationPopups()
